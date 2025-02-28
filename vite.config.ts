b13defcb14060e53a7690cb1aed1caa11a0e567d
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2025-02-28 17:52:05 +0300

    feat: added vite-plugin-compression and code spliting && optimized build

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,63 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import viteCompression from 'vite-plugin-compression'
+import { visualizer } from 'rollup-plugin-visualizer'
 
-// https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [
     vue(),
     vueDevTools(),
-    tailwindcss()
+    tailwindcss(),
+
+    viteCompression({
+      algorithm: 'gzip',
+      threshold: 1024,
+    }),
+    viteCompression({
+      algorithm: 'brotliCompress',
+      threshold: 1024,
+    }),
+
+    mode === 'production' && visualizer({ open: false }),
   ],
+
+  build: {
+    target: 'esnext',
+    minify: 'esbuild',
+    terserOptions: {
+      compress: {
+        drop_console: mode === 'production',
+        drop_debugger: mode === 'production',
+      },
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('vue')) return 'vue'
+            return 'vendor'
+          }
+        },
+      },
+    },
+  },
+
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'pinia'],
+    exclude: ['some-heavy-lib'],
+  },
+
   test: {
     environment: 'jsdom',
     globals: true,
   },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
-})
+}))
