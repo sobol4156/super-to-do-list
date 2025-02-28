9083b3ba5982d0448e7e3c47e0e68c1097885394
DESKTOP-6CDM0P7\sobol <sobol4156@gmail.com>
2025-02-28 12:07:35 +0300

    feat: added infinity scroll

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,15 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { ref, computed, onMounted } from 'vue'
 import TaskInput from '@/components/TaskInput.vue'
 import TaskItem from '@/components/TaskItem.vue'
 import TaskFilters from '@/components/TaskFilters.vue'
 import { useTaskStore } from '@/store/taskStore'
 
 const taskStore = useTaskStore()
-const searchQuery = ref('')
 
 const filter = ref<'all' | 'active' | 'completed'>('all')
+const searchQuery = ref('')
+const visibleTasks = ref(15)
 
 const filteredTasks = computed(() => {
   let tasks = taskStore.tasks
@@ -27,12 +28,28 @@ const filteredTasks = computed(() => {
 
   return tasks
 })
+
+const paginatedTasks = computed(() => filteredTasks.value.slice(0, visibleTasks.value))
+
+const loadMoreTasks = () => {
+  if (visibleTasks.value < filteredTasks.value.length) {
+    visibleTasks.value += 15
+  }
+}
+
+const handleScroll = () => {
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+    loadMoreTasks()
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
 </script>
 
 <template>
-  <div
-    class="container max-w-lg mx-auto p-6 bg-[#1e1e1e] shadow-lg rounded-lg flex flex-col gap-[10px]"
-  >
+  <div class="container max-w-lg mx-auto p-6 bg-[#1e1e1e] shadow-lg rounded-lg flex flex-col gap-[10px]">
     <h1 class="text-2xl font-bold text-center">To-Do List</h1>
 
     <TaskInput />
@@ -47,9 +64,9 @@ const filteredTasks = computed(() => {
     <TaskFilters v-model="filter" />
 
     <ul class="flex flex-col gap-[8px]">
-      <TaskItem v-for="task in filteredTasks" :key="task.id" :task="task" />
+      <TaskItem v-for="task in paginatedTasks" :key="task.id" :task="task" />
     </ul>
+
+    <p v-if="paginatedTasks.length === 0" class="text-gray-400 text-center mt-4">❌ Задачи не найдены</p>
   </div>
 </template>
-
-<style scoped></style>
